30068585becdb73e4a5c81c917a18fe1be05211a
张仕春 <zhangshichun_looper@aliyun.com>
2026-03-03 07:06:14 +0800

    feat: add graph print

---
+++ lesson_14/02_langchain_print.py
@@ -0,0 +1,27 @@
+import os
+from langchain.agents import create_agent
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+# 加载环境变量
+load_dotenv()
+
+def get_weather(city: str) -> str:
+    """获取给定城市的天气情况"""
+    return f"{city}未来10天很热，温度大概45°C"
+
+llm = ChatOpenAI(
+    model="deepseek-chat",
+    api_key=os.getenv("DEEP_SEEK_API_KEY"),
+    base_url=os.getenv("DEEP_SEEK_API_URL"),
+    temperature=0.7
+)
+
+agent = create_agent(
+    model=llm,
+    tools=[get_weather],
+    system_prompt="你是一个经验丰富的天气预测员",
+)
+
+# 直接在终端里用字符画出 Agent 的流转图
+agent.get_graph().print_ascii()
